288bdcfffbb6d47d9e3941c1696476116fb8b048
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-22 01:58:19 -0300

    feat: Implementar autenticación de usuario con formulario de inicio de sesión y manejo de errores

--- safewaters-dashboard/src/app/pages/login/login.component.ts
+++ safewaters-dashboard/src/app/pages/login/login.component.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {
+  FormGroup,
+  FormsModule,
+  FormControl,
+  FormGroupDirective,
+  ReactiveFormsModule,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+import { AuthService } from '../../services/auth.service';
+import {Router} from '@angular/router';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
+    ReactiveFormsModule, FormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = signal(
+    new FormGroup({
+      usernameOrEmail: new FormControl('user@example.com', { nonNullable: true, validators: [Validators.required] }),
+      password: new FormControl('aStrongPassword123!', { nonNullable: true, validators: [Validators.required] })
+    })
+  );
 
+
+  matcher = new MyErrorStateMatcher();
+  hide = signal(true);
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+
+
+  onSubmit(): void {
+    if (this.loginForm().valid) {
+      const { usernameOrEmail, password } = this.loginForm().value;
+      let credentials: LoginRequest = { password: password! };
+
+      if (usernameOrEmail!.includes('@')) {
+        credentials.email = usernameOrEmail;
+      } else {
+        credentials.username = usernameOrEmail;
+      }
+
+      this.authService.login(credentials).subscribe({
+        next: (response) => {
+          localStorage.setItem('token', response.access_token);
+          this.router.navigate(['/dashboard']);
+          console.log('Login exitoso:', response);
+        },
+        error: (err) => {
+          console.error('Error en login:', err);
+        },
+      });
+    } else {
+      console.error('Formulario inválido');
+    }
+  }
+}
+export interface LoginRequest {
+  username?: string;
+  email?: string;
+  password: string;
 }
--- safewaters-dashboard/src/app/services/auth.service.ts
+++ safewaters-dashboard/src/app/services/auth.service.ts
@@ -1,9 +1,85 @@
 import { Injectable } from '@angular/core';
+import { catchError, throwError, Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly apiUrl = '/api';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router
+  ) { }
+
+  register(userData: RegisterRequest): Observable<RegisterResponse> {
+    return this.httpClient.post<RegisterResponse>(`${this.apiUrl}/api/auth/register`, userData)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  login(credentials: LoginRequest): Observable<LoginResponse> {
+    const body = new HttpParams()
+      .set('grant_type', 'password')
+      .set('username', credentials.username || credentials.email || '')
+      .set('password', credentials.password);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    return this.httpClient.post<LoginResponse>(
+      `${this.apiUrl}/api/auth/login`,
+      body.toString(),
+      { headers }
+    ).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('access_token');
+    this.router.navigate(['/login']);
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'An unknown error occurred!';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    }
+    );
+  }
+}
+
+export interface RegisterRequest {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export interface RegisterResponse {
+  email: string;
+  username: string;
+  id: string;
+  created_at: string;
+}
+
+export interface LoginRequest {
+  username?: string;
+  email?: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  access_token: string;
+  token_type: string;
 }
